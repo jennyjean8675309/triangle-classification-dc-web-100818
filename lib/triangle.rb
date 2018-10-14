1044595663b25f5bd6ef0474e163472e4565419e
jennyjean <jennyjean8675309@yahoo.com>
2018-10-14 14:49:38 -0400

    Created a custom error message for Triangle class

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+
+  attr_accessor :length_1, :length_2, :length_3
+
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def kind
+    if (@length_1 + @length_2 > @length_3 == false) || (@length_2 + @length_3 > @length_1 == false) || (@length_1 + @length_3 > @length_2 == false) || (@length_1 > 0 == false) || (@length_2 > 0 == false) || (@length_3 > 0 == false)
+    raise TriangleError
+    elsif @length_1 == @length_2 && @length_2 == @length_3
+      :equilateral
+    elsif (@length_1 == @length_2 && @length_1 != @length_3) || (@length_1 == @length_3 && @length_1 != @length_2) || (@length_2 == @length_3 && @length_2 != @length_1)
+      :isosceles
+    elsif (@length_1 != @length_2 && @length_2 != @length_3)
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
